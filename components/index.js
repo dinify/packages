1bcaa4ae169e20e562226a1ba56ebc5926e18412
Máté Homolya <mate.homolya@gmail.com>
2019-09-27 17:10:22 +0200

    Update folder

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 const core = require('@dinify/core');
 
-console.log('This is github.com/dinify/packages/core')
+console.log('This is github.com/dinify/packages/components')
 core.helloWorld();
 
 module.exports = core;
